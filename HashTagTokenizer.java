f62b3310384af486c8416da6d81a2f817ae4b594
Ohad Cohen <ohad.cohen02@post.runi.ac.il>
2024-02-01 19:05:42 +0200

    Ohad's Homework 7

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -11,29 +11,36 @@ public class HashTagTokenizer {
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i = 0; i < dictionary.length; i++)
+		{
+			dictionary[i] = in.readString();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++)
+		{
+			if (word.equals(dictionary[i]))
+				return true;
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
+		hashtag = hashtag.toLowerCase();
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
             return;
         }
- 
         int N = hashtag.length();
-
         for (int i = 1; i <= N; i++) {
-		
+			if (existInDictionary(hashtag.substring(0,i),dictionary) == true) {
+				System.out.println(hashtag.substring(0,i));
+				breakHashTag(hashtag.substring(i, N),dictionary);
+				break;
+			}
         }
     }
 
